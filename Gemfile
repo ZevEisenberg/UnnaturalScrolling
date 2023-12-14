98be212fb1b28efc0d2207e587ae19290226ad15
Zev Eisenberg <zev@zeveisenberg.com>
2023-12-13 21:30:27 -0500

    Migrate from CocoaPods to SPM. Stop importing BonMot, which I was not actually using.

--- Gemfile
+++
@@ -1,2 +0,0 @@
-gem 'cocoapods'
-gem 'fastlane'
--- Pods/Target Support Files/Pods-Hackathon/Pods-Hackathon-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hackathon : NSObject
-@end
-@implementation PodsDummy_Pods_Hackathon
-@end
